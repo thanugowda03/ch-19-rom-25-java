26b92bdfd841bbf03d3b7ea5883e7eca59727029
thanugowda03 <144573436+thanugowda03@users.noreply.github.com>
2025-06-30 16:17:18 +0530

    file

---
+++ Gym.java
@@ -0,0 +1,92 @@
+class Gym {
+    static String gymName;
+    static String location;
+    static String contactNumber;
+    static String email;
+    static String ownerName;
+    static String gymType;
+    static String membershipPlan;
+    static String timings;
+    static String website;
+    static String address;
+    static String facilities;
+    static String safetyMeasures;
+    static int numberOfTrainers;
+    static int numberOfMembers;
+    static int numberOfEquipment;
+    static String membershipFeeMonthly;  
+    static String membershipFeeYearly;  
+    static int numberOfFloors;
+    static int maxCapacity;
+    static int yearEstablished;
+
+    public static void main(String[] bodybuilder) {
+    System.out.println("main started");
+		
+	System.out.println("Gym Name: " + gymName);
+    System.out.println("Location: " + location);
+    System.out.println("Contact Number: " + contactNumber);
+    System.out.println("Email: " + email);
+    System.out.println("Owner Name: " + ownerName);
+    System.out.println("Gym Type: " + gymType);
+    System.out.println("Membership Plan: " + membershipPlan);
+    System.out.println("Timings: " + timings);
+    System.out.println("Website: " + website);
+    System.out.println("Address: " + address);
+    System.out.println("Facilities: " + facilities);
+    System.out.println("Safety Measures: " + safetyMeasures);
+    System.out.println("Number of Trainers: " + numberOfTrainers);
+    System.out.println("Number of Members: " + numberOfMembers);
+    System.out.println("Number of Equipment: " + numberOfEquipment);
+    System.out.println("Membership Fee (Monthly):" + membershipFeeMonthly);
+    System.out.println("Membership Fee (Yearly):" + membershipFeeYearly);
+    System.out.println("Number of Floors: " + numberOfFloors);
+    System.out.println("Max Capacity: " + maxCapacity);
+    System.out.println("Year Established: " + yearEstablished);
+		
+	//re-Init	
+	String gymName = "PowerHouse Gym";
+    String location = "Koramangala, Bangalore";
+    String contactNumber = "080-1234-5678";
+    String email = "contact@powerhousegym.in";
+    String ownerName = "Rajesh Kumar";
+    String gymType = "Fitness & Strength Training";
+    String membershipPlan = "Monthly, Quarterly, Yearly";
+    String timings = "5:00 AM - 11:00 PM";
+    String website = "www.powerhousegym.in";
+    String address = "123, 5th Block, Koramangala, Bangalore - 560034";
+    String facilities = "Cardio, Weightlifting, Yoga, Personal Training";
+    String safetyMeasures = "Sanitization, Social Distancing";
+    int numberOfTrainers = 12;
+    int numberOfMembers = 500;
+    int numberOfEquipment = 150;
+    String membershipFeeMonthly = "1500Rs";  
+    String membershipFeeYearly = "15000Rs";  
+    int numberOfFloors = 2;
+    int maxCapacity = 100;
+    int yearEstablished = 2015;
+
+        System.out.println("Gym Name: " + gymName);
+        System.out.println("Location: " + location);
+        System.out.println("Contact Number: " + contactNumber);
+        System.out.println("Email: " + email);
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Gym Type: " + gymType);
+        System.out.println("Membership Plan: " + membershipPlan);
+        System.out.println("Timings: " + timings);
+        System.out.println("Website: " + website);
+        System.out.println("Address: " + address);
+        System.out.println("Facilities: " + facilities);
+        System.out.println("Safety Measures: " + safetyMeasures);
+        System.out.println("Number of Trainers: " + numberOfTrainers);
+        System.out.println("Number of Members: " + numberOfMembers);
+        System.out.println("Number of Equipment: " + numberOfEquipment);
+        System.out.println("Membership Fee (Monthly):" + membershipFeeMonthly);
+        System.out.println("Membership Fee (Yearly):" + membershipFeeYearly);
+        System.out.println("Number of Floors: " + numberOfFloors);
+        System.out.println("Max Capacity: " + maxCapacity);
+        System.out.println("Year Established: " + yearEstablished);
+
+        System.out.println("main ended");
+    }
+}
---
+++ SwitchBoard.java
@@ -0,0 +1,92 @@
+class SwitchBoard {
+    static String brand;
+    static String modelName;
+    static String colour;
+    static String material;
+    static String switchType;
+    static String itemShape;
+    static String finishType;
+    static String specialFeature;
+    static String voltageSupport;
+    static String warranty;
+    static int price;
+    static int numberOfSwitches;
+    static int numberOfSockets;
+    static int numberOfIndicators;
+	static int numberOfPorts;
+	static double ratings;
+    static String currentCapacity; 
+    static int yearOfManufacture;
+    static String width; 
+    static String height;
+	
+    public static void main(String[] philips) {
+        System.out.println("main started");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model Name: " + modelName);
+        System.out.println("Colour: " + colour);
+        System.out.println("Material: " + material);
+        System.out.println("Switch Type: " + switchType);
+        System.out.println("Item Shape: " + itemShape);
+        System.out.println("Finish Type: " + finishType);
+        System.out.println("Special Feature: " + specialFeature);
+        System.out.println("Voltage Support: " + voltageSupport);
+        System.out.println("Warranty: " + warranty);
+        System.out.println("Price:"  + price);
+        System.out.println("Number of Switches: " + numberOfSwitches);
+        System.out.println("Number of Sockets: " + numberOfSockets);
+        System.out.println("Number of Indicators: " + numberOfIndicators);
+		System.out.println("Number of ports: " + numberOfPorts);
+	    System.out.println("Ratings :" + ratings);
+        System.out.println("Current Capacity: " + currentCapacity);
+        System.out.println("Year of Manufacture: " + yearOfManufacture);
+        System.out.println("Board Width: " + width);
+        System.out.println("Board Height: " + height);
+		
+		//re-Init
+        String brand = "Philips";
+        String modelName = "Modular Switch Board 6x6";
+        String colour = "White";
+        String material = "Polycarbonate";
+        String switchType = "Modular";
+        String itemShape = "Square";
+        String finishType = "Glossy";
+        String specialFeature = "Shock Proof";
+        String voltageSupport = "240V";
+        String warranty = "2 Years";
+        int price = 799;
+        int numberOfSwitches = 6;
+        int numberOfSockets = 2;
+        int numberOfIndicators = 1;
+		int numberOfPorts = 4;
+		double ratings = 4.5;
+        String currentCapacity = "16 Amps"; 
+        int yearOfManufacture = 2024;
+        String width = "6 inches"; 
+        String height = "6 inches";
+
+		
+        System.out.println("Brand: " + brand);
+        System.out.println("Model Name: " + modelName);
+        System.out.println("Colour: " + colour);
+        System.out.println("Material: " + material);
+        System.out.println("Switch Type: " + switchType);
+        System.out.println("Item Shape: " + itemShape);
+        System.out.println("Finish Type: " + finishType);
+        System.out.println("Special Feature: " + specialFeature);
+        System.out.println("Voltage Support: " + voltageSupport);
+        System.out.println("Warranty: " + warranty);
+        System.out.println("Price:"  + price);
+        System.out.println("Number of Switches: " + numberOfSwitches);
+        System.out.println("Number of Sockets: " + numberOfSockets);
+        System.out.println("Number of Indicators: " + numberOfIndicators);
+		System.out.println("Number of ports: " + numberOfPorts);
+	    System.out.println("Ratings :" + ratings);
+        System.out.println("Current Capacity: " + currentCapacity);
+        System.out.println("Year of Manufacture: " + yearOfManufacture);
+        System.out.println("Board Width: " + width);
+        System.out.println("Board Height: " + height);
+
+        System.out.println("main ended");
+    }
+}
