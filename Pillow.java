44c6a40447efdf47b69ac0cc88b2793745e688e3
thanugowda03 <144573436+thanugowda03@users.noreply.github.com>
2025-06-30 14:12:47 +0530

    file

---
+++ Pillow.java
@@ -0,0 +1,37 @@
+class Pillow{
+
+static int price = 877;
+static String brand = "Wakefit";
+static String colour = "White,Grey";
+static String fillMaterial = "Polyester";
+static String pillowType =	"Bed Pillow";
+static String size	= "Pack of 2";
+static String shape = "Rectangle";
+static String coverMaterial = "Polyester";
+static String pattern = "Striped";
+	
+public static void main( String women[]){
+System.out.println("main started");
+
+// int price = 877;
+// String brand = "Wakefit";
+// String colour = "White,Grey";
+// String fillMaterial = "Polyester";
+// String pillowType =	"Bed Pillow";
+// String size	= "Pack of 2";
+// String shape = "Rectangle";
+// String coverMaterial = "Polyester";
+// String pattern = "Striped";
+
+System.out.println("Price is "+price);
+System.out.println("Brand is "+brand);
+System.out.println("Colour is "+ colour);
+System.out.println("Filled Material is "+fillMaterial);
+System.out.println("Pillow Type is "+ pillowType);
+System.out.println("Size of the pillow is "+size);
+System.out.println("Shape of the pillow is "+size);
+System.out.println("Cover Material is " +coverMaterial);
+System.out.println("Pattern of the pillow is "+coverMaterial);
+System.out.println("main ended");
+}
+}
